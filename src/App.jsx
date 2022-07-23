c61df9f0dccdc84da3aa35154fba60faa993115c
Diganta Nayak <diganta.19032001@gmail.com>
2022-07-23 13:19:34 +0530

    history functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,42 @@ import { calculateWinner } from './components/helpers';
 import './styles/root.scss';
 
 const App = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXnext, setXnext] = useState(false);
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isXnext: true },
+  ]);
+  const [currentMove, setCurrentMove] = useState(0);
 
-  const winner = calculateWinner(board);
+  const current = history[currentMove];
+  const winner = calculateWinner(current.board);
 
   const message = winner
     ? `Winner is ${winner}`
-    : `Next player is ${isXnext ? 'X' : 'O'}`;
+    : `Next player is ${current.isXnext ? 'X' : 'O'}`;
 
   const handelOnClick = position => {
-    if (board[position] || winner) {
+    if (current.board[position] || winner) {
       return;
     }
 
-    setBoard(prev => {
-      return prev.map((valueXorO, pos) => {
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((valueXorO, pos) => {
         if (pos === position) {
-          return isXnext ? 'X' : 'O';
+          return last.isXnext ? 'X' : 'O';
         }
         return valueXorO;
       });
+      return prev.concat({ board: newBoard, isXnext: !last.isXnext });
     });
-    setXnext(prev => !prev);
+    setCurrentMove(prev => prev + 1);
   };
 
   return (
     <div className="app">
       <h1>Tic Tac Toe Game</h1>
       <h2>{message}</h2>
-      <Board handelOnClick={handelOnClick} board={board} />
+      <Board handelOnClick={handelOnClick} board={current.board} />
     </div>
   );
 };
